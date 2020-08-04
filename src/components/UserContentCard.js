554b4435bd6ec5ff69840bf89e1a695ba5612a5f
Javier Fong <fong.kayyuen@gmail.com>
2020-08-04 21:43:59 +0800

    Added UserContentCard

---
+++ src/components/UserContentCard.js
@@ -0,0 +1,51 @@
+import React, { Component, Fragment } from 'react'
+import { MDBCol, MDBRow, MDBBtn, MDBCard, MDBCardBody,MDBCardTitle, MDBCardImage, MDBIcon, MDBCardText, MDBCardFooter } from 'mdbreact'
+
+class UserContentCard extends Component {
+    render() {
+        return (
+            <Fragment>
+                <MDBCard style={{ width: "22rem" }} className='m-4'>
+                    {/*contenttype === puns? if true render text : if false render image*/ }
+                    <MDBCardImage
+                        style={{ width: '100%', height: 'auto' }}
+                        className="img-fluid mx-auto"
+                        src={this.props.imgUrl}
+                        waves>
+                    </MDBCardImage>
+                    <MDBCardTitle className='mx-2 p-2 border-bottom'>{this.props.pun}</MDBCardTitle>
+                    <MDBCardBody style={{ flex: '0 1 auto' }}>
+                        <MDBCardText>
+                            {this.props.caption}
+                            </MDBCardText>
+                        <MDBBtn href="#" size="sm">Edit</MDBBtn> {/*edit route*/}
+                        <MDBBtn href="#" size="sm">delete</MDBBtn>{/*delete route*/}
+                    </MDBCardBody>
+                    <MDBRow className='mx-0 p-2 justify-content-center align-items-end' style={{ flex: '1 1 auto' }}>
+                        <MDBCol>
+                            <MDBIcon icon="share" size="lg" />
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBRow className='mx-auto justify-content-center'>
+                                <MDBIcon icon="thumbs-up" size="lg" className="m-auto align-self-center" />
+                                <h5 className="font-weight-light m-auto"> {this.props.likeAmt}</h5>
+                            </MDBRow>
+                        </MDBCol>
+                        <MDBCol>
+                            <MDBRow className='mx-auto justify-content-center'>
+                                <MDBIcon icon="comment-dots" size="lg" className="m-auto align-self-center" />
+                                <h5 className="font-weight-light m-auto align-self-center"> {this.props.commentAmt}</h5>
+                            </MDBRow>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBCardFooter color="grey lighten-1" >
+                        posted by, 
+                        <a href='/username' className='text-decoration-none'>{this.props.postedBy}</a>
+                    </MDBCardFooter>
+                </MDBCard>
+            </Fragment>
+        )
+    }
+}
+
+export default UserContentCard
--- src/components/UserPuns.js
+++ src/components/UserPuns.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from 'react'
-import ContentCard from '../components/contentCard';
+import UserContentCard from '../components/UserContentCard';
 
 class UserPuns extends Component {
     render() {
         return (
             <Fragment>
-                <ContentCard caption={this.props.caption} pun={this.props.pun} postedBy={this.props.postedBy} />
+                <UserContentCard caption={this.props.caption} pun={this.props.pun} postedBy={this.props.postedBy} />
             </Fragment>
         )
     }
